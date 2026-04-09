a18c57e5c37b5d37587575d703faa18d5d2d9ab1
Joel Muñoz <israjmr19@gmail.com>
2026-04-09 10:41:57 -0500

    Optimizado para web app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development", // Se desactiva en dev para que no moleste la caché
+  register: true,
+  workbox: {
+    skipWaiting: true,
+  },
+} as any);
+
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  turbopack: {},
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
